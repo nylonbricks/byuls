4fe415c76856853faefa98a4a9cbd1603c5f8368
ohprettyhak <me@haklee.me>
2025-08-16 01:32:00 +0900

    feat(website): integrate react-markdown and remark-gfm for enhanced discussion rendering

--- website/src/app/[locale]/widget/page.tsx
+++ website/src/app/[locale]/widget/page.tsx
@@ -1,5 +1,7 @@
 import { notFound } from 'next/navigation';
-import { Fragment } from 'react';
+import Markdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import { twMerge } from 'tailwind-merge';
 import { getDiscussions } from '~/api/discussions';
 
 type Props = {
@@ -15,11 +17,27 @@ const Page = async ({ searchParams }: Props) => {
   const data = await getDiscussions(repoId, categoryId);
 
   return (
-    <Fragment>
+    <main className="flex flex-col gap-4">
       {data.map(discussion => {
-        return <div key={discussion.id}>{discussion.title}</div>;
+        return (
+          <div className="rounded-md border border-neutral-100 px-4 py-2" key={discussion.id}>
+            <h2 className="font-bold text-lg">{discussion.title}</h2>
+            <Markdown
+              remarkPlugins={[remarkGfm]}
+              components={{
+                h1: ({ node, ...props }) => <h1 className="font-bold text-3xl" {...props} />,
+                h2: ({ node, ...props }) => <h2 className="font-semibold text-2xl" {...props} />,
+                h3: ({ node, ...props }) => <h3 className="font-semibold text-xl" {...props} />,
+                h4: ({ node, ...props }) => <h4 className="font-medium text-lg" {...props} />,
+                p: ({ node, ...props }) => <p className="" {...props} />,
+              }}
+            >
+              {discussion.body}
+            </Markdown>
+          </div>
+        );
       })}
-    </Fragment>
+    </main>
   );
 };
 
